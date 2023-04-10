13227f00d357fa863918b14f5ffa5898cd5c3093
SwarajKotapati <kotapatiswaraj06@gmail.com>
2023-04-10 23:41:51 +0530

    One way and Two way data binding Demo

---
+++ EmployeeBlazorServerProject/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeBlazorServerProject.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected String Name { get; set; } = "Sathya Damera";
+        protected String Gender { get; set; } = "Male";
+
+        protected String Color { get; set; } = "background-color: white";
+    }
+}
