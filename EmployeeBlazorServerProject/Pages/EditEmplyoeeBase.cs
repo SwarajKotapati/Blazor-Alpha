e0d126bee9db68876243f31024e6f78a3778c8c9
SwarajKotapati <kotapatiswaraj06@gmail.com>
2023-04-11 05:18:15 +0530

    Building Form and binding the data

---
+++ EmployeeBlazorServerProject/Pages/EditEmplyoeeBase.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeBlazorServerProject.Services;
+using EmployeeDetails.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeBlazorServerProject.Pages
+{
+    public class EditEmplyoeeBase : ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+        
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(Id);
+        }
+
+
+    }
+}
