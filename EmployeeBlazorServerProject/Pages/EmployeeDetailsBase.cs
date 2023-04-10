666e73412cab4f4eae14b4bb98711d093357e39c
SwarajKotapati <kotapatiswaraj06@gmail.com>
2023-04-10 20:47:10 +0530

    Event Handling using @onmousemove and @onclick

--- EmployeeBlazorServerProject/Pages/EmployeeDetailsBase.cs
+++ EmployeeBlazorServerProject/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeBlazorServerProject.Services;
 using EmployeeDetails.Model;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeBlazorServerProject.Pages
@@ -17,9 +18,35 @@ namespace EmployeeBlazorServerProject.Pages
 
         protected string FontFamily = "Verdana";
 
+        protected string Cordinates { get; set; }
+
+        protected string CordinatesClass { get; set; }
+
+        protected string CordinatesButttonText { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
             Employee = await _IemployeeService.GetEmployee(int.Parse(Id));
+            CordinatesButttonText = "Show Cordinates";
+            CordinatesClass = "hideDisplay";
+        }
+
+        protected void MouseCordinates(MouseEventArgs mouseEventArgs)
+        {
+            Cordinates =  $" X = {mouseEventArgs.ClientX}  + Y = {mouseEventArgs.ClientY}";
+        }
+
+        protected void DisplayCordinates()
+        {
+            if(CordinatesClass == null)
+            {
+                CordinatesClass = "hideDisplay";
+            }
+            else
+            {
+                CordinatesClass = null;
+                CordinatesButttonText = "Hide Cordinates";
+            }
         }
     }
 }
