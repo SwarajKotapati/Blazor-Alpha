0f1d62db0845174b92d4324d64fc2dc76d0bb11f
SwarajKotapati <kotapatiswaraj06@gmail.com>
2023-04-11 01:26:32 +0530

    Simple dynamic textarea using two way databinding

--- EmployeeBlazorServerProject/Pages/DataBindingDemoBase.cs
+++ EmployeeBlazorServerProject/Pages/DataBindingDemoBase.cs
@@ -8,5 +8,9 @@ namespace EmployeeBlazorServerProject.Pages
         protected String Gender { get; set; } = "Male";
 
         protected String Color { get; set; } = "background-color: white";
+
+        protected String Description { get; set; } = "";
+
+        protected int Count { get; set; } = 0;
     }
 }
