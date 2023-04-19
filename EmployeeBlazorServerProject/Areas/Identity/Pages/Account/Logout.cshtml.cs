98b8b40a072a22c49341589c27015075c6923744
SwarajKotapati <kotapatiswaraj06@gmail.com>
2023-04-19 12:56:23 +0530

    Logout Login chaining bug fixed

--- EmployeeBlazorServerProject/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ EmployeeBlazorServerProject/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -25,6 +25,8 @@ namespace EmployeeBlazorServerProject.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
+            returnUrl ??= Url.Content("~/");
+
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
